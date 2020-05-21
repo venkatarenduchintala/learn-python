2957006d57cff8e6bdd2e824092ca7c56b7f358a
venkatarenduchintala <12938054+venkatarenduchintala@users.noreply.github.com>
2020-05-21 23:52:10 +0200

    Update Chapter4.py

--- src/python/Chapter4.py
+++ src/python/Chapter4.py
@@ -2,5 +2,42 @@
 import turtle;
 bob = turtle.Turtle();
 print(bob);
-turtle.mainloop();
+#turtle.mainloop();
+# simple repitition
+
+for i in range(4):
+    print('Hello!');
+
+# R
+bob.fd(100);
+bob.lt(90);
+bob.fd(100);
+bob.lt(90);
+bob.fd(100);
+bob.lt(90);
+bob.fd(300);
+bob.pu();
+bob.lt(180);
+bob.fd(200);
+bob.pd();
+
+bob.rt(135);
+
+bob.fd(150);
+
+bob.pu();
+
+bob.lt(180);
+bob.fd(400);
+
+bob.pd();
+
+for i in range (4):
+    bob.fd(100);
+    bob.lt(90);
+
+
+
+
+
 
